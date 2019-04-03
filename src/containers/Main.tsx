0f958de2015a85faa5fd426db4f5b8b043f92505
ptlergo <ptlergo@gmail.com>
2019-04-03 01:20:00 -0400

    import new container comps

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
+import Form from "./Form";
+import Tasks from "./Tasks";
+
 const Main = () => {
-  return <main className="container">hello</main>;
+  return <main className="container">
+    <header>
+      <h1>ToDo App</h1>
+      <Form />
+    </header>
+    <Tasks />
+  </main>;
 };
 
 export default Main;
