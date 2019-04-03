fe82eabb73dc716787b13440332e9b3acadd1d43
ptlergo <ptlergo@gmail.com>
2019-04-03 01:27:46 -0400

    init onclick function, use input comp

--- src/containers/Form.tsx
+++ src/containers/Form.tsx
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
+import Input from "../components/Input";
+import { IFormProps } from "./Main";
 
-const Form = () => {
-  return <div>form container</div>;
+export default (props: IFormProps) => {
+
+  const [value, setValue] = useState("");
+  const { addTask } = props;
+
+  /**
+   * @description on subm
+   * @param e 
+   */
+  const handleSubmit = (e: any): void => {
+    e.preventDefault();
+    addTask('test')
+  };
+
+  return (
+    <form>
+      <Input value={value} setValue={setValue} />
+      <button onClick={handleSubmit}></button>
+    </form>
+  );
 };
-
-export default Form;
