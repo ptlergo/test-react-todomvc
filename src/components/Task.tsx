6a2f9cec5f94dcb975c9e94ef1f65bb04bdf78fd
ptlergo <ptlergo@gmail.com>
2019-04-03 02:00:39 -0400

    button comp, mouse events for hover, task numbering

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,10 +1,31 @@
 import React from "react";
+
 import { ITaskProps } from "../containers/Tasks";
+
+import Button from "./Button";
 
 export default (props: ITaskProps) => {
   return (
-    <article className="tasks__item card">
-            <p className="card-text">{props.detail}</p>
+    <article className="tasks__item card"
+    onMouseEnter={() => props.onEnter()}
+    onMouseLeave={() => props.onLeave()}
+    >
+      <header className="card-header">
+        <h3>Task #{props.index + 1}</h3>
+        <h4 className="badge badge-success">
+          {props.isCompleted && "completed"}
+        </h4>
+      </header>
+      <div className="card-body">
+        <p className="card-text">{props.detail}</p>
+        <div className="actions-box">
+          <Button
+            type={`${props.hover} btn btn-danger`}
+            action={props.deleteTask}
+            text={"delete"}
+          />
+        </div>
+      </div>
     </article>
   );
 };
