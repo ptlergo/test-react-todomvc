40635bd88a65e928c15fd95967980dc33c9e2133
ptlergo <ptlergo@gmail.com>
2019-04-03 01:38:41 -0400

    interfaces for tasks comp. state update func.

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -2,27 +2,51 @@ import React, { useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 
+export interface ITasksProps {
+  tasksObj: {
+    tasksArr: [{ detail: string; isCompleted: boolean; hover: string }];
+    tasksAmnt: number;
+  };
+}
+
 export interface IFormProps {
   addTask(detail: string): void;
 }
 
 const Main = () => {
+  const initTasksObj: any = {
+    tasksArr: [],
+    tasksAmnt: 0
+  };
+
+  // init state for tasks
+  const [tasksObj, setTasksObj] = useState(initTasksObj);
 
   /**
    * @description add task from input
-   * @param detail 
+   * @param detail
    */
   const addTask = (detail: string): void => {
-    console.log({detail})
+    const newTasks: any = [
+      ...tasksObj.tasksArr,
+      { detail, isCompleted: false, hover: "hidden" }
+    ];
+
+    setTasksObj({
+      tasksArr: newTasks,
+      tasksAmnt: newTasks.length
+    });
   };
 
-  return <main className="container">
-    <header>
-      <h1>ToDo App</h1>
-      <Form addTask={addTask} />
-    </header>
-    <Tasks />
-  </main>;
+  return (
+    <main className="container">
+      <header>
+        <h1>ToDo App</h1>
+        <Form addTask={addTask} />
+      </header>
+      <Tasks tasksObj={tasksObj} />
+    </main>
+  );
 };
 
 export default Main;
