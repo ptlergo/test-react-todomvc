fc4b945b09ab3b2affb24c4c096995795d6a7ba6
ptlergo <ptlergo@gmail.com>
2019-04-03 01:28:10 -0400

    addtask func pass as prop

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -2,11 +2,24 @@ import React, { useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 
+export interface IFormProps {
+  addTask(detail: string): void;
+}
+
 const Main = () => {
+
+  /**
+   * @description add task from input
+   * @param detail 
+   */
+  const addTask = (detail: string): void => {
+    console.log({detail})
+  };
+
   return <main className="container">
     <header>
       <h1>ToDo App</h1>
-      <Form />
+      <Form addTask={addTask} />
     </header>
     <Tasks />
   </main>;
