cc728e7afbb6564d1cf4e530c18a2cbeeb82eada
ptlergo <ptlergo@gmail.com>
2019-04-03 01:46:01 -0400

    interface for task comp. conditional for rendering tasks and amnt

--- src/containers/Tasks.tsx
+++ src/containers/Tasks.tsx
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
+
 import { ITasksProps } from "./Main";
+
+import Task from "../components/Task";
+
+export interface ITaskProps {
+  key: number;
+  index: number;
+  detail: string;
+  hover: string;
+  isCompleted: boolean;
+}
 
 const Tasks = (props: ITasksProps) => {
   const { tasksArr, tasksAmnt } = props.tasksObj;
@@ -7,15 +18,26 @@ const Tasks = (props: ITasksProps) => {
   const renderTask = (): any =>
     tasksArr.map((task: any, index: number) => {
       return (
-        <h1 key={index}>{task.detail}</h1>
+        <Task
+          key={index}
+          detail={task.detail}
+          index={index}
+          hover={task.hover}
+          isCompleted={task.isCompleted}
+        />
       );
     });
-
-  return <>
-    <ul>
-      {renderTask()}
-    </ul>
-  </>;
+  return (
+    <>
+      {tasksAmnt > 0 && (
+        <section className="tasks">
+          <span className="badge badge-pill badge-info">{tasksAmnt}</span>{" "}
+          <strong>Task{tasksAmnt > 1 ? "s " : " "} Left</strong>
+          <ul>{renderTask()}</ul>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default Tasks;
