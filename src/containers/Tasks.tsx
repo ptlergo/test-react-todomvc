6a0acd967e1ca06456d9db3fb1ebb378cc704a59
ptlergo <ptlergo@gmail.com>
2019-04-03 01:39:19 -0400

    display tasks items inputed

--- src/containers/Tasks.tsx
+++ src/containers/Tasks.tsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
+import { ITasksProps } from "./Main";
 
-const Tasks = () => {
-  return <div>tasks container</div>;
+const Tasks = (props: ITasksProps) => {
+  const { tasksArr, tasksAmnt } = props.tasksObj;
+
+  const renderTask = (): any =>
+    tasksArr.map((task: any, index: number) => {
+      return (
+        <h1 key={index}>{task.detail}</h1>
+      );
+    });
+
+  return <>
+    <ul>
+      {renderTask()}
+    </ul>
+  </>;
 };
 
 export default Tasks;
